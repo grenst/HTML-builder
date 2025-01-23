648666c85f259f7cb71edf9847dfb8bed18ad0e2
grenst <henkshredder@gmail.com>
2025-01-23 15:08:56 +0100

    fix: changed closing script process

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,24 +1,34 @@
 const fs = require('fs');
 const path = require('path');
-const process = require('process');
+const readline = require('readline');
 
 const outputPath = path.join(__dirname, 'output.txt');
 const writeStream = fs.createWriteStream(outputPath, { flags: 'a' });
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 console.log('Please enter text to write to the file. Type "exit" to quit.');
 
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-
-  if (input === 'exit') {
-    console.log('Exiting...');
-    process.exit();
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    gracefulShutdown();
   } else {
     writeStream.write(`${input}\n`);
   }
 });
 
-process.on('SIGINT', () => {
-  console.log('Exiting...');
-  process.exit();
+rl.on('SIGINT', () => {
+  gracefulShutdown();
 });
+
+function gracefulShutdown() {
+  console.log('\nExiting...');
+
+  writeStream.end(() => {
+    rl.close();
+    process.exit(0);
+  });
+}
